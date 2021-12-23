79be9b7b5df8853efba4145a8225155604e430d6
gitinitkranthi <kranthisura0@gmail.com>
2021-12-23 17:09:00 +0530

    myfirstpycode

---
+++ firstpython.py
@@ -0,0 +1,15 @@
+x = float(input("enter the number"))
+def plusorminus():
+    if x == 0:
+        print("zero")
+    elif x > 0:
+        print("positive")
+    else:
+        print("negetive")
+plusorminus()
+def evenorodd():
+    if(x % 2) == 0:
+        print("even")
+    else:
+        print("odd")
+evenorodd()
